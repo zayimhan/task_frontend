3084ff2e87dcd2ca5375f4a92dc43ab49b7a0ad5
zayimhan <zayimhankorkmaz@gmail.com>
2026-01-31 01:50:13 +0300

    adres frontende taşındı

--- src/app/core/services/address.service.ts
+++ src/app/core/services/address.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { environment } from '../../../enviroments/enviroment.prod';
+import { ADDRESS_DATA } from '../constants/address-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AddressService {
-  private apiUrl = `${environment.apiUrl}/address`;
+  /*private apiUrl = `${environment.apiUrl}/address`;
 
   constructor(private http: HttpClient) {}
 
@@ -17,5 +18,13 @@ export class AddressService {
 
   getStatesByCountry(countryId: number): Observable<any> {
     return this.http.get(`${this.apiUrl}/states/${countryId}`);
+  }*/
+  getCountries(): Observable<any[]> {
+    return of(ADDRESS_DATA);
+  }
+
+  getStatesByCountry(countryId: number): Observable<any[]> {
+    const country = ADDRESS_DATA.find((c) => c.id == countryId);
+    return of(country ? country.states : []);
   }
 }
